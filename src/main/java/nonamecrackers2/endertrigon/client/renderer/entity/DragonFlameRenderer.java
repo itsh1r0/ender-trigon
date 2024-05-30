1e3a64dd4054d1d7bea713505f4e422a9acf475e
nonamecrackers2 <nonamecrackers2@outlook.com>
2024-05-30 13:47:29 -0600

    Clean up

--- src/main/java/nonamecrackers2/endertrigon/client/renderer/entity/DragonFlameRenderer.java
+++ src/main/java/nonamecrackers2/endertrigon/client/renderer/entity/DragonFlameRenderer.java
@@ -16,9 +16,6 @@
 
 package nonamecrackers2.endertrigon.client.renderer.entity;
 
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
-
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
